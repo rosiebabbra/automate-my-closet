e29102db5c02f7ca4f29068cf73d4f1d825a0865
Rosie Babbra <rosie.babbra@nielsen.com>
2020-05-12 19:29:37 -0700

    Introduce react application with basic image output

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import sweatpant from './sweatpant.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={sweatpant} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
